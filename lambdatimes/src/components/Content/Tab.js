fd68ef6a8ef26b165b0e20b75e4ea890845a08f7
J A <jarata@gmail.com>
2019-01-25 10:19:49 -0800

    this is where I get stuck

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,6 +5,9 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  if (props.tab === props.selectedTab) {
+
+  }
   return (
     <div
       className={''}
